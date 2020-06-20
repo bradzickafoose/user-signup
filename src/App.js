07d1706a3b1054ad736cf0e807c1d2e71cd4aa06
Brad <brad@viziworx.com>
2020-06-20 14:18:08 -0400

    imports UserSignup component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import UserSignup from './UserSignup';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>User Signup</h1>
       </header>
+      <UserSignup />
     </div>
   );
 }
